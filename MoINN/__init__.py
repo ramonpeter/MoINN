a41294d15f2663f6a8f2ca3208a62f1518b8989e
Ramon Winterhalder <winterhalder@thphys.uni-heidelberg.de>
2021-04-23 19:58:20 +0200

    Add init files

---
+++ MoINN/__init__.py
@@ -0,0 +1,3 @@
+from . import modules
+
+__all__ = ["modules"]
---
+++ MoINN/modules/__init__.py
@@ -0,0 +1,8 @@
+# Import the base class first
+from .base import *
+
+# Then all inheriting modules
+from .coupling_layers import *
+from .fixed_permutation import *
+from .subnetworks import *
+from .all_in_one_block import *
