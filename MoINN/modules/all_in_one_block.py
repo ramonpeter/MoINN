859169f1387a21f06400eea859554e057c97a596
Ramon Winterhalder <rpw@mailbox.org>
2021-11-11 10:15:12 +0100

    Fix splitting of network output

--- MoINN/modules/all_in_one_block.py
+++ MoINN/modules/all_in_one_block.py
@@ -1,5 +1,6 @@
 """All in one coupling Block"""
 
+from re import I
 import warnings
 from typing import Dict, Union
 
@@ -213,7 +214,7 @@ class AllInOneBlock(InvertibleModule):
         # the entire coupling coefficient tensor is scaled down by a
         # factor of ten for stability and easier initialization.
         a *= 0.1
-        s, t = tf.split(a, [self.splits[0], self.splits[1]], -1)
+        s, t = tf.split(a, [self.splits[1], self.splits[1]], -1)
         sub_jac = self.clamp * tf.math.tanh(s)
         if self.GIN:
             sub_jac -= tf.reduce_mean(sub_jac, axis=self.sum_dims, keepdims=True)
